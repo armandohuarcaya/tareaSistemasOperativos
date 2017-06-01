dafb9091113e6e5d39fb90bf9a7e0d3efa7f1a82
armandohuarcaya <armando_huarcaya@upeu.edu.pe>
2017-05-31 22:40:02 -0500

    commit

--- hola.sh
+++ hola.sh
@@ -8,3 +8,5 @@ git push -u origin master
 
 echo "Viernes Feliz Sabado"
 echo "Domingo, Lunes, Martes, Mierscoles, Jueves"
+echo "Amigos de github estamos aqui subiendo una modificacion mas en el repositorio de git mediante crontab -e y nano hola.sh"
+
